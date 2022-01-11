559af7e3226bbe39c86bdb9fbc7194b73d6a4470
lupusg <just_vlad@yahoo.com>
2022-01-11 20:26:53 +0200

    Add mysql module

--- src/main/application/module-info.java
+++ src/main/application/module-info.java
@@ -2,6 +2,7 @@ module com.lpsv.fancycarrental {
   requires javafx.controls;
   requires javafx.fxml;
   requires de.jensd.fx.glyphs.fontawesome;
+  requires java.sql;
 
   opens controllers to javafx.fxml;
   exports controllers;
