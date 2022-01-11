4979eaa999d35d2e85f4cc2fdf476f213466af6d
lupusg <just_vlad@yahoo.com>
2022-01-11 12:59:20 +0200

    Update design

--- src/main/application/controllers/LoginSceneController.java
+++ src/main/application/controllers/LoginSceneController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -13,8 +14,8 @@ public class LoginSceneController extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(LoginSceneController.class.getResource("login.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+    Parent root = FXMLLoader.load(getClass().getResource("login/login.fxml"));
+    Scene scene = new Scene(root, 800, 500);
     scene.setFill(Color.TRANSPARENT);
     stage.initStyle(StageStyle.TRANSPARENT);
     stage.setResizable(false);
