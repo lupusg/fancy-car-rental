6c40c3a46c736dab83c53aa1d58a8c1ff22bc367
lupusg <just_vlad@yahoo.com>
2022-01-11 20:25:47 +0200

    Add custom title bar with close and minimize buttons

---
+++ src/main/application/controllers/Main.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class Main {
+  private double x, y;
+
+  @FXML
+  void dragged(MouseEvent event) {
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+    stage.setX(event.getScreenX() - x);
+    stage.setY(event.getScreenY() - y);
+  }
+
+  @FXML
+  void close(MouseEvent event) {
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    stage.close();
+  }
+
+  @FXML
+  void minimize(MouseEvent event) {
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    stage.setIconified(true);
+  }
+
+  @FXML
+  void pressed(MouseEvent event) {
+    x = event.getSceneX();
+    y = event.getSceneY();
+  }
+}
