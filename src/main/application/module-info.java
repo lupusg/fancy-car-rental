99e914446b729dcafd679d2151d23195a6115d05
lupusg <just_vlad@yahoo.com>
2022-01-12 21:40:28 +0200

    Delete mysql module

--- src/main/application/module-info.java
+++ src/main/application/module-info.java
@@ -2,7 +2,6 @@ module com.lpsv.fancycarrental {
   requires javafx.controls;
   requires javafx.fxml;
   requires de.jensd.fx.glyphs.fontawesome;
-  requires java.sql;
   requires mongo.java.driver;
 
   opens controllers to javafx.fxml;
