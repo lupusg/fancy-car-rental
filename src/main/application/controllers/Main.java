c464fd5d0e457c28e9dc97c3a94ede1412ef9a14
lupusg <just_vlad@yahoo.com>
2022-01-14 11:25:09 +0200

    Fix weird margin in gridpane

--- src/main/application/controllers/Main.java
+++ src/main/application/controllers/Main.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import com.mongodb.DocumentToDBRefTransformer;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -10,13 +13,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+import logic.DatabaseConnection;
 import model.Car;
+import org.bson.Document;
+import org.json.JSONObject;
 
 public class Main implements Initializable {
   private double x, y;
@@ -24,6 +31,8 @@ public class Main implements Initializable {
   @FXML private GridPane grid;
 
   @FXML private ScrollPane scroll;
+
+  @FXML private Button carsBtn;
 
   @FXML
   void dragged(MouseEvent event) {
@@ -51,17 +60,26 @@ public class Main implements Initializable {
     y = event.getSceneY();
   }
 
+  @FXML
+  void toMyCars(){
+
+  }
+
   private List<Car> cars = new ArrayList<>();
 
   private List<Car> getData() {
     List<Car> cars = new ArrayList<>();
+    DatabaseConnection databaseConnection = new DatabaseConnection("car_rental", "cars");
+    MongoCollection<Document> collection = databaseConnection.getCollection();
+    MongoCursor<Document> cursor = collection.find().iterator();
     Car car;
 
-    for (int i = 0; i < 5; ++i) {
+    while (cursor.hasNext()) {
+      JSONObject carJson = new JSONObject(cursor.next().toJson());
       car = new Car();
-      car.setName("Maybach S60" + i);
-      car.setPrice("de la 999 RON/zi");
-      car.setImgSrc("/images/mercedes_limo.png");
+      car.setName(carJson.get("name").toString());
+      car.setPrice(carJson.get("price").toString());
+      car.setImgSrc(carJson.get("imgSrc").toString());
       cars.add(car);
     }
 
@@ -72,7 +90,7 @@ public class Main implements Initializable {
   public void initialize(URL url, ResourceBundle resourceBundle) {
     cars.addAll(getData());
     int column = 0;
-    int row = 0;
+    int row = 1;
 
     try {
       for (int i = 0; i < cars.size(); ++i) {
@@ -90,7 +108,7 @@ public class Main implements Initializable {
         }
 
         grid.add(anchorPane, column++, row);
-        GridPane.setMargin(anchorPane, new Insets(250, 50, 0 , 7));
+        GridPane.setMargin(anchorPane, new Insets(10, 40, 0 , 7));
       }
     } catch (IOException e) {
       e.printStackTrace();
