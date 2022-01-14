ec5ab32ff6fc4096dc21ff0f4754f1cfa3b501a9
lupusg <just_vlad@yahoo.com>
2022-01-14 11:24:33 +0200

    Add new cars

--- src/main/application/module-info.java
+++ src/main/application/module-info.java
@@ -3,6 +3,7 @@ module com.lpsv.fancycarrental {
   requires javafx.fxml;
   requires de.jensd.fx.glyphs.fontawesome;
   requires mongo.java.driver;
+  requires org.json;
 
   opens controllers to javafx.fxml;
   exports controllers;
