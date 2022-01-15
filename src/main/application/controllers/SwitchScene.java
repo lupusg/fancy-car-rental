cc897a8f8a1754b9f08528ddf5563713291085b5
lupusg <just_vlad@yahoo.com>
2022-01-15 20:54:33 +0200

    Add switch for login page

--- src/main/application/controllers/SwitchScene.java
+++ src/main/application/controllers/SwitchScene.java
@@ -1,15 +1,29 @@
 package controllers;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class SwitchScene {
   public static void switchScene(String fxmlFile, Button button, int width, int height) throws IOException {
+    Parent root = FXMLLoader.load(SwitchScene.class.getResource(fxmlFile));
+
+    Stage window = (Stage) button.getScene().getWindow();
+    window.setScene(new Scene(root, width, height));
+  }
+
+  public static void switchToLogin(String fxmlFile, Button button) throws IOException {
+    Parent root = FXMLLoader.load(SwitchScene.class.getResource(fxmlFile));
+    Stage window = (Stage) button.getScene().getWindow();
+    window.setScene(LoginScene.loginScene);
+  }
+
+
+  public static void refreshScene(String fxmlFile, ImageView button, int width, int height) throws IOException {
     Parent root = FXMLLoader.load(SwitchScene.class.getResource(fxmlFile));
 
     Stage window = (Stage) button.getScene().getWindow();
