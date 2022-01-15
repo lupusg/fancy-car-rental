6ec6491b3cb9ac1934bfad08177e91e34efcaa26
lupusg <just_vlad@yahoo.com>
2022-01-15 20:59:11 +0200

    Add popup confirmation

--- src/main/application/controllers/Item.java
+++ src/main/application/controllers/Item.java
@@ -2,8 +2,8 @@ package controllers;
 
 import static com.mongodb.client.model.Filters.eq;
 
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Updates;
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -15,19 +15,15 @@ import model.Car;
 import org.bson.Document;
 
 public class Item {
-  @FXML
-  private ImageView img;
+  @FXML private ImageView img;
 
-  @FXML
-  private Label nameLabel;
+  @FXML private Label nameLabel;
 
-  @FXML
-  private Label priceLabel;
-
+  @FXML private Label priceLabel;
 
   private Car car;
 
-  public void setData(Car car){
+  public void setData(Car car) {
     this.car = car;
     nameLabel.setText(car.getName());
     priceLabel.setText(car.getPrice());
@@ -36,16 +32,24 @@ public class Item {
   }
 
   @FXML
-  void onMouseClick(MouseEvent event) {
-    ArrayList<Document> cars = new ArrayList<Document>();
-    Document car = new Document();
+  public void onMouseClick(MouseEvent event) throws IOException, InterruptedException {
 
-    car.put("name", this.car.getName());
-    car.put("price", this.car.getPrice());
-    car.put("imgSrc", this.car.getImgSrc());
+    Popup popup = new Popup();
+    popup.showAndWait();
 
-    cars.add(car);
+    if (Static.answer) {
+      ArrayList<Document> cars = new ArrayList<Document>();
 
-    DatabaseConnection.usersCollection.findOneAndUpdate(eq("_id", Login.username), Updates.pushEach("cars", cars));
+      Document car = new Document();
+
+      car.put("name", this.car.getName());
+      car.put("price", this.car.getPrice());
+      car.put("imgSrc", this.car.getImgSrc());
+
+      cars.add(car);
+
+      DatabaseConnection.usersCollection.findOneAndUpdate(
+          eq("_id", Login.username), Updates.pushEach("cars", cars));
+    }
   }
 }
