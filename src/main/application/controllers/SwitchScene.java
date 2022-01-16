5a56aa92b6a586c613be576f7ec7d89294b35505
lupusg <just_vlad@yahoo.com>
2022-01-16 13:39:38 +0200

    Update transparent switch logic

--- src/main/application/controllers/SwitchScene.java
+++ src/main/application/controllers/SwitchScene.java
@@ -6,7 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class SwitchScene {
   public static void switchScene(String fxmlFile, Button button, int width, int height) throws IOException {
@@ -19,9 +21,20 @@ public class SwitchScene {
   public static void switchToLogin(String fxmlFile, Button button) throws IOException {
     Parent root = FXMLLoader.load(SwitchScene.class.getResource(fxmlFile));
     Stage window = (Stage) button.getScene().getWindow();
-    window.setScene(Main.loginScene);
+    Scene scene = new Scene(root, 800, 500);
+
+    scene.setFill(Color.TRANSPARENT);
+    window.setScene(scene);
   }
 
+  public static void switchToSignUp(String fxmlFile, Button button) throws IOException {
+    Parent root = FXMLLoader.load(SwitchScene.class.getResource(fxmlFile));
+    Stage window = (Stage) button.getScene().getWindow();
+    Scene scene = new Scene(root, 800, 500);
+
+    scene.setFill(Color.TRANSPARENT);
+    window.setScene(scene);
+  }
 
   public static void refreshScene(String fxmlFile, ImageView button, int width, int height) throws IOException {
     Parent root = FXMLLoader.load(SwitchScene.class.getResource(fxmlFile));
